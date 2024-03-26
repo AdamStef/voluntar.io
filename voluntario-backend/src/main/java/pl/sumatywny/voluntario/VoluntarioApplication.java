ce5fed5af65cf1ce2b9cda13924222032d56ca28
Adam Stefański <adamtonki@gmail.com>
2024-03-26 17:10:06 +0100

    Add backend project

---
+++ voluntario-backend/src/main/java/pl/sumatywny/voluntario/VoluntarioApplication.java
@@ -0,0 +1,13 @@
+package pl.sumatywny.voluntario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoluntarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoluntarioApplication.class, args);
+	}
+
+}
---
+++ voluntario-backend/src/test/java/pl/sumatywny/voluntario/VoluntarioApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.sumatywny.voluntario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoluntarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

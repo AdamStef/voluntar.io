771f0c951dda8e3f2d887c3d5bf66f9fadd5fb27
Krystian <242510@edu.p.lodz.pl>
2024-04-15 20:31:05 +0200

    Event, Location classes created and so their respective repository classes

---
+++ voluntario-backend/src/main/java/pl/sumatywny/voluntario/model/event/Event.java
@@ -0,0 +1,40 @@
+package pl.sumatywny.voluntario.model.event;
+
+import pl.sumatywny.voluntario.model.user.User;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Event {
+    private Long id;
+    private String name;
+    private String description;
+    private User organizer;
+    private List<User> participants;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+
+
+    private Boolean addParticipant(User user) {
+        if(participants.contains(user)) {
+            return false;
+        }
+        else {
+            return participants.add(user);
+        }
+    }
+
+    private Boolean removeParticipant(User user) {
+        if(participants.contains(user)) {
+            return participants.remove(user);
+        }
+        else {
+            return false;
+        }
+    }
+}

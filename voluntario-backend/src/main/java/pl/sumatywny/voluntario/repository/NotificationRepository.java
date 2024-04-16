cdbf581df2fcf3c3791ed641224bd02ac38764be
WojtaS52 <wojciechiderski@gmail.com>
2024-04-16 12:15:52 +0200

    added notification - start working

--- voluntario-backend/src/main/java/pl/sumatywny/voluntario/repository/NotificationRepository.java
+++ voluntario-backend/src/main/java/pl/sumatywny/voluntario/repository/NotificationRepository.java
@@ -1,10 +1,11 @@
 package pl.sumatywny.voluntario.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import pl.sumatywny.voluntario.model.notification.Notification;
 
 import java.util.List;
 
-public interface NotificationRepository {
+public interface NotificationRepository extends JpaRepository<Notification, Integer> {
 
     List<Notification> getNotifications();
     void saveNotification(Notification notification);

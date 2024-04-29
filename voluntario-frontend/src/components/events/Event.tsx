6cbc943d430f7a338d3171628b9318194abd4230
Adam Stefański <adamtonki@gmail.com>
2024-04-29 23:33:54 +0200

    Add progress to event bar

--- voluntario-frontend/src/components/events/Event.tsx
+++ voluntario-frontend/src/components/events/Event.tsx
@@ -6,6 +6,7 @@ import { Calendar, MapPin } from 'lucide-react';
 import { Button } from '../ui/button';
 import { Link } from 'react-router-dom';
 import { getLocationString } from '@/utils/helpers';
+import { Progress } from '../ui/progress';
 
 type EventProps = {
   event: EventType;
@@ -16,42 +17,52 @@ export const Event: React.FC<EventProps> = ({ event, className }) => {
   return (
     <div
       className={cn(
-        'flex h-48 max-h-64 gap-4 rounded-sm bg-secondary p-4 text-secondary-foreground',
+        'flex h-48 max-h-64 w-full justify-between gap-4 rounded-sm bg-secondary p-4 text-secondary-foreground',
         className,
       )}
     >
-      <div className="flex grow justify-between">
-        <div className="flex flex-col gap-2">
-          <H3>{event.name}</H3>
+      {/* <div className="flex justify-between"> */}
+      <div className="flex basis-3/5 flex-col gap-2">
+        <H3>{event.name}</H3>
 
-          {/* Date */}
-          <div className="flex items-center">
-            <span className="mr-1">
-              <Calendar className="h-4 w-4" />
-            </span>
-            <p className="text-sm">
-              {event.startDate.toLocaleDateString()}
-              {event.startDate.getDate() !== event.endDate.getDate() && (
-                <span> - {event.endDate.toLocaleDateString()}</span>
-              )}
-            </p>
-          </div>
-
-          {/* Location */}
-          <div className="flex items-center">
-            <span className="mr-1">
-              <MapPin className="h-4 w-4" />
-            </span>
-            <p className="text-sm">{getLocationString(event.location)}</p>
-          </div>
-          {/* <p>{event.description}</p> */}
+        {/* Date */}
+        <div className="flex items-center">
+          <span className="mr-1">
+            <Calendar className="h-4 w-4" />
+          </span>
+          <p className="text-sm">
+            {event.startDate.toLocaleDateString()}
+            {event.startDate.getDate() !== event.endDate.getDate() && (
+              <span> - {event.endDate.toLocaleDateString()}</span>
+            )}
+          </p>
         </div>
 
-        {/* <div className="h-24 w-24 bg-primary">map</div> */}
-        <Button asChild>
-          <Link to={`/events/${event.id}`}>Zobacz więcej</Link>
-        </Button>
+        {/* Location */}
+        <div className="flex items-center">
+          <span className="mr-1">
+            <MapPin className="h-4 w-4" />
+          </span>
+          <p className="text-sm">{getLocationString(event.location)}</p>
+        </div>
+        {/* <p>{event.description}</p> */}
       </div>
+      <div className="flex basis-1/5 flex-col">
+        Liczba uczestników: {event.participants.length}/
+        {event.numberOfVolunteersNeeded}
+        <Progress
+          className=" bg-accent"
+          value={
+            (event.participants.length / event.numberOfVolunteersNeeded) * 100
+          }
+        />
+      </div>
+
+      {/* <div className="h-24 w-24 bg-primary">map</div> */}
+      <Button asChild className="basis-1/5">
+        <Link to={`/events/${event.id}`}>Zobacz więcej</Link>
+      </Button>
     </div>
+    // </div>
   );
 };

9e079c49d0b3d3682c22f6a6f59b27fad36836c1
WojtaS52 <wojciechiderski@gmail.com>
2024-05-03 18:11:31 +0200

    removed unused thing

--- voluntario-frontend/src/pages/Leaderboard.tsx
+++ voluntario-frontend/src/pages/Leaderboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { Navbar } from '../components/navbars/Navbar';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';

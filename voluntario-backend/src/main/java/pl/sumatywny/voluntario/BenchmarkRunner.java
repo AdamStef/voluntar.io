972e3d23d01fab024766f7d5858f9b580d51a775
Adam Stefański <adamtonki@gmail.com>
2024-06-10 20:52:24 +0200

    benchmark tests

---
+++ voluntario-backend/src/main/java/pl/sumatywny/voluntario/BenchmarkRunner.java
@@ -0,0 +1,9 @@
+package pl.sumatywny.voluntario;
+
+import org.openjdk.jmh.Main;
+
+public class BenchmarkRunner {
+    public static void main(String[] args) throws Exception {
+        Main.main(args);
+    }
+}
